22c3ebbcd6f3f5ad7005183d7d6988b1ba2d2ae0
Baccus Li <libaccus@gmail.com>
2026-03-29 22:19:43 +0200

    feat: Implement piece-ghost interaction on the board

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,8 +1,18 @@
+import { Piece, pieces } from "./Pieces";
+
 export default function Board({ ghosts }) {
     const board = Array.from({ length: 4 }, () => Array(4).fill('⬛'));
 
     ghosts.forEach((pos, key) => {
         board[pos.y][pos.x] = '👻';
+    });
+
+    pieces.forEach((piece) => {
+        const matches = piece.lightsGhost(ghosts, 0, 1);
+
+        matches.forEach((match) => {
+        board[match.y][match.x] = "👹";
+        });
     });
 
     return (
